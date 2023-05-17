9cafbebd538c413ec3e4ec23ebaebadac38045a5
Phil Reif <pr@sambal.net>
2023-05-17 12:03:18 +0200

    Cleanup code

--- mune.js
+++ mune.js
@@ -36,18 +36,15 @@ Hooks.on("renderChatLog", async function (chatLog, html, user) {
     const isSidebar = game.settings.get("mune", "windowStyle") === 3;
     if (!isSidebar)
         return;
-
     
     const win = new MuneWindow();
     game.mune.window = win;
     game.mune.sidebar = html;
-    let toPrepend = await renderTemplate("modules/mune/templates/apps/mune.hbs", win.getData());
-    html.prepend(toPrepend);
-    win.activateListeners(html.find("#mune"));
-
     game.mune.window.renderSidebar = async () => {
         game.mune.sidebar.find("#mune").remove();
         game.mune.sidebar.prepend(await renderTemplate("modules/mune/templates/apps/mune.hbs", game.mune.window.getData()));
         game.mune.window.activateListeners(game.mune.sidebar.find("#mune"));
     };
+
+    game.mune.window.renderSidebar();
 });
